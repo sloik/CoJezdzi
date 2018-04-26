1492f213e1ccc92d0a6a5204122db403226c293f
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-26 22:42:07 +0200

    WiP: Adding test/wip code to see what's going on

--- CoJezdzi/CoJezdzi/AppDelegate.swift
+++ CoJezdzi/CoJezdzi/AppDelegate.swift
@@ -1,14 +1,44 @@
 import UIKit
 
+import ReSwift
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var appStore = Store<AppState>(reducer: appReducer, state: nil)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         UINavigationBar.appearance().tintColor = UIColor.eggplant()
+        
 
+        subscribeToStore()
+        DispatchQueue.main.async {
+            self.tempWipCodeForStuff()
+        }
+        
         return true
+    }
+}
+
+extension AppDelegate: StoreSubscriber {
+    func newState(state: AppState) {
+        print(state)
+    }
+}
+
+// MARK: - Temporary WiP code to test thing out
+
+extension AppDelegate {
+
+    func tempWipCodeForStuff() {
+        appStore.dispatch(FetchTramsAction())
+        appStore.dispatch(FetchBussesAction())
+    }
+    
+    func subscribeToStore() {
+        appStore.subscribe(self)
     }
 }
