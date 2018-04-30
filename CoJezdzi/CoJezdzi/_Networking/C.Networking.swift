03624eb8d13d5d9ff3f74e2a5fa14c77398fecf0
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-30 23:21:56 +0200

    Moves networking constants to it's own file.

---
+++ CoJezdzi/CoJezdzi/_Networking/C.Networking.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+extension C {
+    struct Networking {
+        static let URLParamSeperator = "&"
+        
+        struct GoToURL {
+            static let AboutApp      = "https://avantapp.wordpress.com/co-jezdzi/"
+            static let APIUMWarszawa = "https://api.um.warszawa.pl"
+        }
+        
+        struct HTTPMethod {
+            static let GET  = "GET"
+            static let POST = "POST"
+        }
+    }
+}
--- CoJezdzi/CoJezdzi/__Old/Constants.swift
+++ CoJezdzi/CoJezdzi/__Old/Constants.swift
@@ -70,20 +70,6 @@ struct C {
         }
     }
 
-    struct Networking {
-        static let URLParamSeperator = "&"
-
-        struct GoToURL {
-            static let AboutApp      = "https://avantapp.wordpress.com/co-jezdzi/"
-            static let APIUMWarszawa = "https://api.um.warszawa.pl"
-        }
-
-        struct HTTPMethod {
-            static let GET  = "GET"
-            static let POST = "POST"
-        }
-    }
-
     struct Coordinate {
         static let WarsawCenter = CLLocationCoordinate2D(latitude: 52.2429341157752, longitude: 21.0083538438228)
         static let DefaultSpreadDistance: CLLocationDistance = 14000
