5bc45be3784c2a0d808d7447571dd3aedb97f3c2
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-30 21:24:53 +0200

    Removes temporary code.

--- CoJezdzi/CoJezdzi/AppDelegate.swift
+++ CoJezdzi/CoJezdzi/AppDelegate.swift
@@ -4,43 +4,24 @@ import Colours
 import ReSwift
 
 // Global <3 app state ;)
-var appStore = Store<AppState>(reducer: appReducer, state: nil)
+var store = Store<AppState>(reducer: appReducer, state: nil)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var appRouter: AppRouter?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         UINavigationBar.appearance().tintColor = UIColor.eggplant()
         
-
-        subscribeToStore()
-        DispatchQueue.main.async {
-            self.tempWipCodeForStuff()
-        }
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        window.makeKeyAndVisible()
         
+        appRouter = AppRouter(window: window)
+
         return true
-    }
-}
-
-extension AppDelegate: StoreSubscriber {
-    func newState(state: AppState) {
-        print(state)
-    }
-}
-
-// MARK: - Temporary WiP code to test thing out
-
-extension AppDelegate {
-
-    func tempWipCodeForStuff() {
-        appStore.dispatch(FetchTramsAction.fetch)
-        appStore.dispatch(FetchBussesAction.fetch)
-    }
-    
-    func subscribeToStore() {
-        appStore.subscribe(self)
     }
 }
