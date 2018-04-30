452a2f7d496a74da307fe2e898552da8dcaa6c58
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-30 15:55:00 +0200

    make store global :D

--- CoJezdzi/CoJezdzi/AppDelegate.swift
+++ CoJezdzi/CoJezdzi/AppDelegate.swift
@@ -3,13 +3,14 @@ import UIKit
 import Colours
 import ReSwift
 
+// Global <3 app state ;)
+var appStore = Store<AppState>(reducer: appReducer, state: nil)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var appStore = Store<AppState>(reducer: appReducer, state: nil)
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         UINavigationBar.appearance().tintColor = UIColor.eggplant()
