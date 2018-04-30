20fe0233624e0c99ed498b560b58ba7e882284cb
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-01 00:18:01 +0200

    WiP: initial error handeling

--- CoJezdzi/CoJezdzi/_Flux/_Actions/FetchTramsAction.swift
+++ CoJezdzi/CoJezdzi/_Flux/_Actions/FetchTramsAction.swift
@@ -7,9 +7,14 @@ struct FetchTramsAction: Action {
     static func fetch(state: AppState, store: Store<AppState>) -> FetchTramsAction {
         
         WarsawApi.getTrams { result in
-            // TODO: map result to expected type
-            print(#function)
-            store.dispatch(FetchTramsAction(fetched: []))
+            switch result {
+            case .succes(let data):
+                print(data)
+                store.dispatch(FetchTramsAction(fetched: []))
+                
+            case .error(let error):
+                print(error)
+            }            
         }
         
         return FetchTramsAction(fetched: [])
