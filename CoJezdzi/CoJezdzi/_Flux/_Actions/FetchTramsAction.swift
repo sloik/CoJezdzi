afe963f7876c743075b8cbf1f15e191607481e26
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-04-30 23:53:38 +0200

    Initial hook to the api call.

--- CoJezdzi/CoJezdzi/_Flux/_Actions/FetchTramsAction.swift
+++ CoJezdzi/CoJezdzi/_Flux/_Actions/FetchTramsAction.swift
@@ -5,6 +5,13 @@ struct FetchTramsAction: Action {
     let fetched: [WarsawTramDto]
     
     static func fetch(state: AppState, store: Store<AppState>) -> FetchTramsAction {
+        
+        WarsawApi.getTrams { result in
+            // TODO: map result to expected type
+            print(#function)
+            store.dispatch(FetchTramsAction(fetched: []))
+        }
+        
         return FetchTramsAction(fetched: [])
     }
 }
