2986ede3b612d770a856145834968ece09bab715
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-04 13:51:16 +0200

    adds method for getting buses.

--- CoJezdzi/CoJezdzi/_Networking/WarsawApi.swift
+++ CoJezdzi/CoJezdzi/_Networking/WarsawApi.swift
@@ -31,6 +31,33 @@ struct WarsawApi {
         
         task.resume()
     }
+    
+    static func getBusses(completion: @escaping (Result<Any>) -> Void) {
+        
+        let urlComponents = NSURLComponents(string: WarsawApiConstants.BaseURL)!
+        urlComponents.queryItems = [URLQueryItem(name: pKey.ResourceID, value: pValue.ResourceID),
+                                    URLQueryItem(name: pKey.APIKey, value: pValue.APIKey),
+                                    URLQueryItem(name: pKey.TypeKey, value: pValue.TypeBus)]
+        
+        let url = urlComponents.url!
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = C.Networking.HTTPMethod.POST
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            if let error = error  {
+                defer { completion(.error(error)) }
+                return
+            }
+            
+            completion(.succes(data as Any))
+        }
+        
+        task.resume()
+    }
 }
 
 
