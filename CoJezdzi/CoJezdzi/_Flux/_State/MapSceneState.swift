0c1924033ea934883f98a0a30dee3f22c346a526
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-04 14:04:49 +0200

    Adds helper variables/methods.

--- CoJezdzi/CoJezdzi/_Flux/_State/MapSceneState.swift
+++ CoJezdzi/CoJezdzi/_Flux/_State/MapSceneState.swift
@@ -5,3 +5,13 @@ struct MapSceneState: StateType {
     var currentTrams:  VehicleState
     var currentBusses: VehicleState
 }
+
+extension MapSceneState {
+    var allCurrent: [WarsawVehicleDto] {
+        return currentTrams.data + currentBusses.data
+    }
+    
+    var allPrevious: [WarsawVehicleDto] {
+        return currentTrams.previousData + currentBusses.previousData
+    }
+}
