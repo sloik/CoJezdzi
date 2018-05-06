e5e7e83a908391e8d12eb99169b45a2c92a1738a
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-06 10:25:40 +0200

    compacts code

--- CoJezdzi/CoJezdzi/_Redux/_Reducers/MapSceneReducer.swift
+++ CoJezdzi/CoJezdzi/_Redux/_Reducers/MapSceneReducer.swift
@@ -2,13 +2,11 @@
 import ReSwift
 
 func mapSceneReducer(action: Action, state: MapSceneState?) -> MapSceneState {
-    var state: MapSceneState =
+    let state: MapSceneState =
         state ?? MapSceneState(currentTrams: VehicleState(data: [], previousData: []),
                                currentBusses: VehicleState(data: [], previousData: []))
     
-    state = state
+    return state
         .currentTrams (tramsReducer (action: action, state: state.currentTrams ))
         .currentBusses(bussesReducer(action: action, state: state.currentBusses))
-    
-    return state
 }
