783e965a93b69527265e0ca44bee0faf020a1108
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-11 22:45:17 +0200

    Moves logic to seperate file.

--- CoJezdzi/CoJezdzi/_Scenes/_LinesFilter/FilterLinesCVC.swift
+++ CoJezdzi/CoJezdzi/_Scenes/_LinesFilter/FilterLinesCVC.swift
@@ -2,19 +2,6 @@ import UIKit
 
 import ReSwift
 
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-    switch (lhs, rhs) {
-    case let (l?, r?):
-        return l < r
-    case (nil, _?):
-        return true
-    default:
-        return false
-    }
-}
-
 class FilterLinesCVC: UICollectionViewController {
     
     fileprivate var latesState: AppState! {
