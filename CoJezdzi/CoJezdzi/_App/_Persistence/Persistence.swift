817ef8db09514bb063690622f1f3101cb9040b90
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-26 11:18:27 +0200

    Implements loading and persisting of user settings.

--- CoJezdzi/CoJezdzi/_App/_Persistence/Persistence.swift
+++ CoJezdzi/CoJezdzi/_App/_Persistence/Persistence.swift
@@ -2,6 +2,11 @@
 import ReSwift
 
 class Persistence {
+    
+    enum Keys {
+        static let persistance = "Persistance.SettingsState"
+    }
+    
     init() {
         store.subscribe(self) {
             $0.select {
@@ -11,11 +16,15 @@ class Persistence {
     }
     
     func persist(state: SettingsState) {
-        
+        let data = try! JSONEncoder().encode(state)
+        UserDefaults.standard.set(data, forKey: Keys.persistance)
     }
     
-    func load(state: SettingsState) {
+    func load() {
+        let data = UserDefaults.standard.data(forKey: Keys.persistance)!
+        let state = try! JSONDecoder().decode(SettingsState.self, from: data)
         
+        store.dispatch(SettingsDidRestoreAction(restoredState: state))
     }
 }
 
