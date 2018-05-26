3cc3cdf418b8602ecec84e72b301c5e531a90036
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-26 11:29:20 +0200

    Defers observing for new state after old is restored.

--- CoJezdzi/CoJezdzi/_App/_Persistence/Persistence.swift
+++ CoJezdzi/CoJezdzi/_App/_Persistence/Persistence.swift
@@ -7,20 +7,20 @@ class Persistence {
         static let persistance = "Persistance.SettingsState"
     }
     
-    init() {
-        store.subscribe(self) {
-            $0.select {
-                $0.settingsState
-            }
-        }
-    }
-    
     func persist(state: SettingsState) {
         let data = try! JSONEncoder().encode(state)
         UserDefaults.standard.set(data, forKey: Keys.persistance)
     }
     
     func load() {
+        defer {
+            store.subscribe(self) {
+                $0.select {
+                    $0.settingsState
+                }
+            }
+        }
+        
         guard let data = UserDefaults.standard.data(forKey: Keys.persistance) else { return }
         let state = try! JSONDecoder().decode(SettingsState.self, from: data)
         
