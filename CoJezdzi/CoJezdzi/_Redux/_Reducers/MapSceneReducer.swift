c15ac706f8e5eb3752244f1f8b96fa9cde4c57f7
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-05-27 13:01:00 +0200

    Moves logic to common file.

--- CoJezdzi/CoJezdzi/_Redux/_Reducers/MapSceneReducer.swift
+++ CoJezdzi/CoJezdzi/_Redux/_Reducers/MapSceneReducer.swift
@@ -10,3 +10,33 @@ func mapSceneReducer(action: Action, state: MapState?) -> MapState {
         .currentTrams (tramsReducer (action: action, state: state.currentTrams ))
         .currentBusses(bussesReducer(action: action, state: state.currentBusses))
 }
+
+func tramsReducer(action: Action, state: VehicleState?) -> VehicleState {
+    var state = state ?? VehicleState(data: [], previousData: [])
+    
+    switch action {
+    case let fetchActions as FetchTramsAction:
+        state = VehicleState(data: fetchActions.fetched,
+                             previousData: state.data)
+        
+    default:
+        break
+    }
+    
+    return state
+}
+
+func bussesReducer(action: Action, state: VehicleState?) -> VehicleState {
+    var state = state ?? VehicleState(data: [], previousData: [])
+    
+    switch action {
+    case let fetchActions as FetchBussesAction:
+        state = VehicleState(data: fetchActions.fetched,
+                             previousData: state.data)
+        
+    default:
+        break
+    }
+    
+    return state
+}
