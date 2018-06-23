48f9f5a97c4756186961e6277ce3084ce50315e2
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-06-23 14:38:43 +0200

    Stuff

--- CoJezdzi/CoJezdzi/_Redux/_Middleware/AppMiddleware.swift
+++ CoJezdzi/CoJezdzi/_Redux/_Middleware/AppMiddleware.swift
@@ -2,63 +2,54 @@
 import ReSwift
 
 struct M {
-    static func handleResult(result: Result<Any>, actionFactory: (([WarsawVehicleDto]) -> Action), dispatch: @escaping DispatchFunction) {
-        switch result {
-        case .succes(let data):
-            if let dto = try? JSONDecoder().decode(WarsawApiResultDto.self, from: data as! Data) {
-                
-                let withVehicleType =
-                    dto.result.map{ WarsawVehicleDto(latitude: $0.latitude,
-                                                     longitude: $0.longitude,
-                                                     lines: $0.lines,
-                                                     brigade: $0.brigade,
-                                                     time: $0.time,
-                                                     type: .bus) }
-                
-                let action = actionFactory(withVehicleType)
-                DispatchQueue.main.async {
-                    dispatch(action)
-                }
-            } else {
-                debugPrint(#function + "data: \(String(describing: String(data: data as! Data, encoding: .utf8)))")
-            }
-        case .error(let error):
-            print(error)
-        }
-    }
     
     static let Api: Middleware<AppState> = { dispatch, getState in
         return { next in
+            func handleResult(result: Result<Any>, actionFactory: (([WarsawVehicleDto]) -> Action), dispatch: @escaping DispatchFunction) {
+                switch result {
+                case .succes(let data):
+                    if let dto = try? JSONDecoder().decode(WarsawApiResultDto.self, from: data as! Data) {
+                        
+                        let withVehicleType =
+                            dto.result.map{ WarsawVehicleDto(latitude: $0.latitude,
+                                                             longitude: $0.longitude,
+                                                             lines: $0.lines,
+                                                             brigade: $0.brigade,
+                                                             time: $0.time,
+                                                             type: .bus) }
+                        
+                        let action = actionFactory(withVehicleType)
+                        DispatchQueue.main.async {
+                            dispatch(action)
+                        }
+                    } else {
+                        debugPrint(#function + "data: \(String(describing: String(data: data as! Data, encoding: .utf8)))")
+                    }
+                case .error(let error):
+                    print(error)
+                }
+            }
             
             return { action in
-                switch action {
-                case is FetchVehiclesPosytionsAction:
-                    guard let state = getState() else { return }
-                    
-                    let busOnly  = state.settingsState.switches.busOnly.isOn
-                    let tramOnly = state.settingsState.switches.tramOnly.isOn
-                    
-                    if busOnly || tramOnly == false {
-                        // get buses
-                        WarsawApi.getBusses { result in
-                            
-                            handleResult(result: result,
-                                         actionFactory: { return FetchBussesAction(fetched: $0) },
-                                         dispatch: dispatch)
-                        }
-                    }
-                    
-                    if tramOnly || busOnly == false {
-                        WarsawApi.getTrams { result in
-                            
-                            handleResult(result: result,
-                                         actionFactory: { return FetchTramsAction(fetched: $0) },
-                                         dispatch: dispatch)
-                        }
-                    }
-                    
-                default:
-                    return next(action)
+                guard action is FetchVehiclesPosytionsAction, let state = getState() else {
+                    next(action)
+                    return
+                }
+                
+                let busOnly  = state.settingsState.switches.busOnly.isOn
+                let tramOnly = state.settingsState.switches.tramOnly.isOn
+                
+                let fetchBusses = busOnly || tramOnly == false
+                let fetchTrams  = tramOnly || busOnly == false
+                
+                let factory = { return FetchBussesAction(fetched: $0) }
+                
+                if fetchBusses {
+                    WarsawApi.getBusses { handleResult(result: $0, actionFactory: factory, dispatch: dispatch) }
+                }
+                
+                if fetchTrams {
+                    WarsawApi.getTrams { handleResult(result: $0, actionFactory: factory, dispatch: dispatch) }
                 }
             }
         }
