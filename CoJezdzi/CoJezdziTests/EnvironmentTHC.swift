f48cf4acc935c1c721275f5a75d8cbac347648eb
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-08-23 00:32:51 +0200

    WIP: adds mocks

--- CoJezdzi/CoJezdziTests/EnvironmentTHC.swift
+++ CoJezdzi/CoJezdziTests/EnvironmentTHC.swift
@@ -14,16 +14,36 @@ extension Environment {
 
 extension WarsawApi {
     static let mock = WarsawApi(
-        getTrams: { completion in completion(Result.succes(Data())) },
-        getBusses:{ completion in completion(Result.succes(Data()))})
+        getTrams: { completion in completion(.succes(WarsawApiResultDto.mock |> coda))},
+        getBusses:{ completion in completion(.succes(WarsawApiResultDto.mock |> coda))})
     
     
     static let errorMock = WarsawApi(
             getTrams: { completion in
                 let error = NSError(domain: "mock.error.gettingTrams", code: 69, userInfo: nil)
-                completion(Result.error(error)) },
+                completion(.error(error)) },
             
             getBusses:{ completion in
                 let error = NSError(domain: "mock.error.gettingBusses", code: 68, userInfo: nil)
-                completion(Result.error(error))})
+                completion(.error(error))})
+}
+
+extension WarsawVehicleDto {
+    static let mock = WarsawVehicleDto(
+        latitude: 22,
+        longitude: 33,
+        lines: "255",
+        brigade: "brigade",
+        time: "123")
+}
+
+extension WarsawApiResultDto {
+    static let mock = WarsawApiResultDto(
+        result: [.mock]
+    )
+}
+
+
+func coda<C: Codable>(_ c: C) -> Data {
+    return try! JSONEncoder().encode(c)
 }
