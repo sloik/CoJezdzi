e96500f4b39cfdbfaa8d7b41985a8e5d14019fd2
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-08-23 20:33:52 +0200

    Removes unused code.

--- CoJezdzi/CoJezdzi/_Networking/C.Networking.swift
+++ CoJezdzi/CoJezdzi/_Networking/C.Networking.swift
@@ -3,8 +3,6 @@ import Foundation
 
 extension C {
     struct Networking {
-        static let URLParamSeperator = "&"
-        
         struct GoToURL {
             static let AboutApp      = "https://avantapp.wordpress.com/co-jezdzi/"
             static let APIUMWarszawa = "https://api.um.warszawa.pl"
