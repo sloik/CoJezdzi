9a122a2a7ef12bf5086675bbcc9a440333fac108
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-08-24 22:28:06 +0200

    Adds mock for AppState

---
+++ CoJezdzi/CoJezdziTests/_Redux/_State/_Mocks/AppStateMock.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+@testable import CoJezdzi
+
+extension RoutingState {
+    static let mock = RoutingState(scene: .map, destitnation: nil, sceneVC: nil)
+}
+
+extension VehicleState {
+    static let mock = VehicleState(data: [], previousData: [])
+}
+
+extension MapState {
+    static let mock = MapState(currentTrams: .mock,
+                               currentBusses: .mock)
+}
+
+extension SelectedLinesState {
+    static let mock = SelectedLinesState(lines: [])
+}
+
+extension SettingsState.FilterState {
+    static let mock = SettingsState.FilterState(tramOnly: .tram(on: false),
+                                                busOnly: .bus(on: false),
+                                                previousLocations: .previousLocation(on: true))
+}
+
+extension SettingsState {
+    static let mock = SettingsState(selectedLines: .mock,
+                                    switches: .mock)
+}
+
+extension AppState {
+    static let mock = AppState(routingState: .mock,
+                               mapState: .mock,
+                               settingsState:.mock )
+}
