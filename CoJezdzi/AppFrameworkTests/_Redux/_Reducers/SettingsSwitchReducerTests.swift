fbb6c1d6d2ac6865b5742f75d702a71c5948d980
Lukasz Stocki <stocki.lukasz@gmail.com>
2018-09-13 18:52:33 +0200

    Fixes unit test.

--- CoJezdzi/AppFrameworkTests/_Redux/_Reducers/SettingsSwitchReducerTests.swift
+++ CoJezdzi/AppFrameworkTests/_Redux/_Reducers/SettingsSwitchReducerTests.swift
@@ -12,14 +12,17 @@ class SettingsSwitchReducerTests: XCTestCase {
         let action = SettingsSwitchAction(whitchSwitch: .tram(on: true))
 
         // act
-        let r = settingsSwitchReducer(action: action, state: filterState)
+        let reduced = settingsSwitchReducer(action: action, state: filterState)
 
         // assert
-        XCTAssert(r.tramOnly.isOn, "Should have reduce state so the switch is on but it's off!")
-        XCTAssert(r.tramOnly.ty, )
-//        XCTAssert(r.busOnly.isOn == true, "For 'tram only' filter the 'bus only' should be off but its on!!")
+        XCTAssertTrue(
+            reduced.tramOnly.isOn,
+            "Should have reduce state so the switch is on but it's off!"
+        )
+
+        XCTAssertFalse(
+            reduced.busOnly.isOn,
+            "For 'tram only' filter the 'bus only' should be off but its on!!"
+        )
     }
 }
-
-//                 set(\.tramOnly,.tram(on: on ? false : state.tramOnly.isOn))
-
