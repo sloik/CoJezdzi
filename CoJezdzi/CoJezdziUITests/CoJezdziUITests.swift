f4c3669a77a84f4c019f04729bb9cbb12345e9e6
Lukasz Stocki <lukasz.stocki@allegro.pl>
2019-03-14 13:00:18 +0100

    Moves POC to another file.

--- CoJezdzi/CoJezdziUITests/CoJezdziUITests.swift
+++ CoJezdzi/CoJezdziUITests/CoJezdziUITests.swift
@@ -6,49 +6,6 @@ import Overture
 
 class CoJezdziUITests: XCTestCase {
     
-    var application: Void
-    
-
-    
-    override func setUp() {
-        super.setUp()
-        continueAfterFailure = false
-        app.launchTunnel()
-        app.performCustomCommandNamed("setMocks", object: nil)
-        
-    }
-    
-    override func tearDown() {
-        
-        super.tearDown()
-    }
-    
-    func testExample() {
-        let labels: Constants.UI.Settings.MenuLabels = with(
-            Constants.UI.Settings.MenuLabels(),
-            concat(
-                set(\Constants.UI.Settings.MenuLabels.aboutApp, "about about"),
-                set(\Constants.UI.Settings.MenuLabels.tramMarks, "marks marks"),
-                set(\Constants.UI.Settings.MenuLabels.bussesOnly, "AAA"),
-                set(\Constants.UI.Settings.MenuLabels.tramsOnly, "TTTT"),
-                set(\Constants.UI.Settings.MenuLabels.filters, "FFFF"))
-        )
-
-        let labelsData = try! JSONEncoder().encode(labels)
-
-            let objReturnedByBlock = app
-                .performCustomCommandNamed("dupak",
-                                           object: labelsData)
-
-
-        let box = app.staticTexts["LineId"].firstMatch
-        wait(forElement: box,timeout: 20)
-
-        XCTAssertEqual(box.label, "FFFF")
-
-        debugPrint("ddasda")
-
-    }
 }
 
 extension XCTestCase {
