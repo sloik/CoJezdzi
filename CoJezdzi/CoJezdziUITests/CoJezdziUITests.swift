54c15ab7e8a560ed3f72bbbd7dc81af05f68ae21
Lukasz Stocki <lukasz.stocki@allegro.pl>
2019-03-15 09:28:38 +0100

    iPhone Xs

--- CoJezdzi/CoJezdziUITests/CoJezdziUITests.swift
+++ CoJezdzi/CoJezdziUITests/CoJezdziUITests.swift
@@ -5,9 +5,14 @@ import SnapshotTesting
 
 @testable import AppFramework
 
+
+enum TunelCommand: String {
+    case gotoScene = "gotoSceneCommand"
+    case getCurrentScene = "getCurrentScene"
+}
+
+
 class CoJezdziUITests: XCTestCase {
-
-
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
@@ -16,83 +21,75 @@ class CoJezdziUITests: XCTestCase {
                          startupBlock: nil)
     }
     
-    override func tearDown() {
-        
-        super.tearDown()
-    }
 
-    func testExample1() {
-        let labels: Constants.UI.Settings.MenuLabels = with(
-            Constants.UI.Settings.MenuLabels(),
-            concat(
-                set(\Constants.UI.Settings.MenuLabels.aboutApp, "about about"),
-                set(\Constants.UI.Settings.MenuLabels.tramMarks, "marks marks"),
-                set(\Constants.UI.Settings.MenuLabels.bussesOnly, "AAA"),
-                set(\Constants.UI.Settings.MenuLabels.tramsOnly, "TTTT"),
-                set(\Constants.UI.Settings.MenuLabels.filters, "FFFF"))
-        )
-
-        let labelsData = try! JSONEncoder().encode(labels)
-
-        let objReturnedByBlock = app
-            .performCustomCommandNamed("setMocks",
-                                       object: labelsData)
-
-        let box = app.staticTexts["LineId"].firstMatch
-        wait(forElement: box,timeout: 20)
-
-        XCTAssertEqual(box.label, "Wybierz Linie")
-
-        debugPrint("ddasda")
-
-    }
 
     func test_inifinity() {
-        let gotoSceneCommand = "gotoSceneCommand"
-        let getCurrentScene = "getCurrentScene"
-
-
+        record = true
+        var count = 0
         repeat {
-            // wiem dokad
-            let randomDestination = RoutingDestination.allCases.randomElement()!
+            count += 1
 
 
-            // ustawic stan dla destynacji
-            
+            randomDestination
+                .map(navigateTo)
+                .flatMap { sleep(5) }
+                .flatMap {_ in currentScene }
+                .map{ snapshot($0) }
 
 
-            // id tam!
-            _ = app.performCustomCommandNamed(gotoSceneCommand,
-                                              object:  randomDestination.rawValue)
 
-            sleep(5)
-
-            // funcRandomActionForDestiantion(randomDestination)
-            let currrentScene = app.performCustomCommandNamed(getCurrentScene,
-                                                              object: nil) as! String
-
-            let crd = RoutingDestination(rawValue: currrentScene)!
-
-            switch crd {
-            case .map:
-                assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBarAndTimerIndycator!, as: .image(precision: 0.975), named: "map")
-            case .settings:
-                assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBar!, as: .image, named: "settings")
-            case .linesFilter:
-               assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBar!, as: .image, named: "linesFilter")
-            case .aboutApp:
-                sleep(5)
-                assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBar!, as: .image(precision: 0.9), named: "aboutApp")
-            }
-
-
+            debugPrint("😎 \(count)")
         } while true
     }
 }
+
+
+extension CoJezdziUITests {
+    var currentScene: RoutingDestination? {
+        let sceneId = app.perform(.getCurrentScene, nil) as! String
+        return RoutingDestination(rawValue: sceneId)
+    }
+
+
+    var randomDestination: RoutingDestination? {
+        return RoutingDestination.allCases.randomElement()
+    }
+
+    func navigateTo( _ destination: RoutingDestination) {
+        _ = app
+            .perform(.gotoScene,
+                     destination.rawValue)
+    }
+
+    func snapshot(_ destination: RoutingDestination,
+                  _ testName: String = #function) {
+        switch destination {
+        case .map:
+            assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBarAndTimerIndycator!, as: .image(precision: 0.975), named: testName + "-map")
+        case .settings:
+            assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBar!, as: .image, named: testName + "-settings")
+        case .linesFilter:
+            assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBar!, as: .image, named: testName + "-linesFilter")
+        case .aboutApp:
+            sleep(5)
+            assertSnapshot(matching: app.windows.firstMatch.screenshot().image.removingStatusBar!, as: .image(precision: 0.9),
+                           named: testName + "-aboutApp")
+        }
+    }
+}
+
 extension XCTestCase {
     func wait(forElement element: XCUIElement, timeout: TimeInterval) {
         let predicate = NSPredicate(format: "exists == 1")
         expectation(for: predicate, evaluatedWith: element)
         waitForExpectations(timeout: timeout)
+    }
+}
+
+extension SBTUITunneledApplication {
+    func perform(_ command: TunelCommand,
+                 _ object: Any?) -> Any? {
+        return performCustomCommandNamed(command.rawValue,
+                                         object: object)
     }
 }
